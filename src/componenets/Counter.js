5adea9ef9448b4d822432412a6b04136c08ec74e
miray <miray.mutlu@ozu.edu.tr>
2022-08-10 17:28:06 +0300

    increment and decrement functions added

--- src/componenets/Counter.js
+++ src/componenets/Counter.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { increment,decrement } from "../redux/counter/counterSlice";
+
 
 function Counter() {
   const countValue = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
   return (
     <div>
       <h1>{countValue}</h1>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
     </div>
   );
 }
--- src/redux/counter/counterSlice.js
+++ src/redux/counter/counterSlice.js
@@ -5,7 +5,15 @@ export const counterSlice = createSlice({
     initialState: {
         value:0,
     },
-    reducers:{},
+    reducers:{
+        increment: (state) => {
+            state.value += 1;
+        },
+        decrement: (state) => {
+            state.value -= 1;
+        },
+    },
 });
 
+export const {increment,decrement} = counterSlice.actions;
 export default counterSlice.reducer;
